9e144573a6af3f29b7370363cdc6ac0e41dd6c35
edwardgurney <edwardgurney1979@gmail.com>
2021-08-18 17:31:23 +0000

    Initial Commit

---
+++ name.py
@@ -0,0 +1,70 @@
+boy_names = [
+                "Jack", "Oliver", "James", "Charlie", "Harris", "Lewis",
+                "Leo", "Noah", "Alfie", "Rory", "Alexander", "Max",
+                "Logan", "Lucas", "Harry", "Theo", "Thomas", "Brodie",
+                "Archie", "Jacob", "Finlay", "Finn", "Daniel", "Joshua",
+                "Oscar", "Arthur", "Hunter", "Ethan", "Mason", "Harrison",
+                "Freddie", "Ollie", "Adam", "William", "Jaxon", "Aaron",
+                "Cameron", "Liam", "George", "Jamie", "Callum", "Matthew",
+                "Muhammad", "Caleb", "Nathan", "Tommy", "Carter", "Blake",
+                "Andrew", "Luke", "Jude", "Angus", "Riley", "Luca", "Samuel",
+                "Joseph", "David", "Isaac", "Ryan", "Hamish", "Sonny", "Arlo",
+                "Arran", "Cole", "Robert", "Blair", "Dylan", "Louie",
+                "Ruaridh", "Connor", "Benjamin", "Kai", "Michael", "Jackson",
+                "Leon", "Cooper", "Louis", "Theodore", "Fraser", "Owen",
+                "Rueben", "John", "Carson", "Innes", "Eliyah", "Murray",
+                "Grayson", "Aiden", "Alex", "Cody", "Elliot", "Ben", "Henry"
+            ]
+
+girl_names = [
+                "Olivia", "Emily", "Isla", "Sophie", "Ella", "Ava", "Amelia"
+                "Grace", "Freya", "Charlotte", "Jessica", "Lucy", "Ellie"
+                "Sophia", "Aria", "Lily", "Harper", "Mia", "Rosie", "Millie",
+                "Evie", "Eilidh", "Ruby", "Willow", "Anna", "Maisie", "Hannah",
+                "Eva", "Chloe", "Mila", "Orla", "Isabella", "Ivy", "Emma",
+                "Georgia", "Poppy", "Robyn", "Daisy", "Zara", "Gracie",
+                "Holly", "Skye", "Esme", "Sofia", "Erin", "Hallie", "Molly",
+                "Ayla", "Emilia", "Layla", "Katie", "Sienna", "Niamh", "Alice",
+                "Amber", "Bonnie", "Zoe", "Maya", "Ada", "Hollie", "Bella",
+                "Luma",
+                "Thea", "Rose", "Abigail", "Summer", "Callie", "Hope", "Lexi",
+                "Iona", "Elsie", "Leah", "Scarlett", "Julia", "Violet",
+                "Myla", "Harley", "Eve", "Imogen", "Elizabeth", "Cora",
+                "Florence", "Georgie", "Lilly", "Matilda", "Mirren",
+                "Phoebe", "Rowan", "Lola", "Aurora",
+                "Evelyn", "Brooke", "Clara"
+            ]
+
+both_names = [
+                "Jack", "Oliver", "James", "Charlie", "Harris", "Lewis",
+                "Leo", "Noah", "Alfie", "Rory", "Alexander", "Max",
+                "Logan", "Lucas", "Harry", "Theo", "Thomas", "Brodie",
+                "Archie", "Jacob", "Finlay", "Finn", "Daniel", "Joshua",
+                "Oscar", "Arthur", "Hunter", "Ethan", "Mason", "Harrison",
+                "Freddie", "Ollie", "Adam", "William", "Jaxon", "Aaron",
+                "Cameron", "Liam", "George", "Jamie", "Callum", "Matthew",
+                "Muhammad", "Caleb", "Nathan", "Tommy", "Carter", "Blake",
+                "Andrew", "Luke", "Jude", "Angus", "Riley", "Luca", "Samuel",
+                "Joseph", "David", "Isaac", "Ryan", "Hamish", "Sonny", "Arlo",
+                "Arran", "Cole", "Robert", "Blair", "Dylan", "Louie",
+                "Ruaridh", "Connor", "Benjamin", "Kai", "Michael", "Jackson",
+                "Leon", "Cooper", "Louis", "Theodore", "Fraser", "Owen",
+                "Rueben", "John", "Carson", "Innes", "Eliyah", "Murray",
+                "Grayson", "Aiden", "Alex", "Cody", "Elliot", "Ben", "Henry"
+                "Olivia", "Emily", "Isla", "Sophie", "Ella", "Ava", "Amelia"
+                "Grace", "Freya", "Charlotte", "Jessica", "Lucy", "Ellie"
+                "Sophia", "Aria", "Lily", "Harper", "Mia", "Rosie", "Millie",
+                "Evie", "Eilidh", "Ruby", "Willow", "Anna", "Maisie", "Hannah",
+                "Eva", "Chloe", "Mila", "Orla", "Isabella", "Ivy", "Emma",
+                "Georgia", "Poppy", "Robyn", "Daisy", "Zara", "Gracie",
+                "Holly", "Skye", "Esme", "Sofia", "Erin", "Hallie", "Molly",
+                "Ayla", "Emilia", "Layla", "Katie", "Sienna", "Niamh", "Alice",
+                "Amber", "Bonnie", "Zoe", "Maya", "Ada", "Hollie", "Bella",
+                "Luma",
+                "Thea", "Rose", "Abigail", "Summer", "Callie", "Hope", "Lexi",
+                "Iona", "Elsie", "Leah", "Scarlett", "Julia", "Violet",
+                "Myla", "Harley", "Eve", "Imogen", "Elizabeth", "Cora",
+                "Florence", "Georgie", "Lilly", "Matilda", "Mirren",
+                "Phoebe", "Rowan", "Lola", "Aurora",
+                "Evelyn", "Brooke", "Clara"
+]
---
+++ names.py
@@ -0,0 +1,42 @@
+boy_names = [
+                “Jack”, “Oliver”, “James”, “Charlie”, “Harris”, “Lewis”,
+                “Leo”, “Noah”, “Alfie”, “Rory”, “Alexander”, “Max”, “Logan”,
+                “Lucas”,
+                “Harry”, “Theo”, “Thomas”, “Brodie”, “Archie”, “Jacob”, “Finlay”,
+                “Finn”, “Daniel”, “Joshua”, “Oscar”, “Arthur”, “Hunter”, “Ethan”,
+                “Mason”, “Harrison”, “Freddie”, “Ollie”, “Adam”, “William”,
+                “Jaxon”,
+                “Aaron”, “Cameron”, “Liam”, “George”, “Jamie”,
+                “Callum”, “Matthew”, “Muhammad”, “Caleb”, “Nathan”,
+                “Tommy”, “Carter”, “Blake”, “Andrew”, “Luke”, “Jude”, “Angus”,
+                “Riley”, “Luca”, “Samuel”, “Joseph”, “David”, “Isaac”, “Ryan”,
+                “Hamish”, “Sonny”, “Arlo”, “Arran”, “Cole”,
+                “Robert”, “Blair”, “Dylan”, “Louie”, “Ruaridh”, “Connor”,
+                “Benjamin”, “Kai”, “Michael”,
+                “Jackson”, “Leon”, “Cooper”, “Louis”, “Theodore”,
+                “Fraser", “Owen", “Reuben”, “John”, “Carson”, “Innes”,
+                “Elijah”, “Murray”, “Grayson”,
+                “Aiden”, “Alex”, “Cody”, “Elliot", “Ben”, “Henry”
+]
+
+girls_name = str.[Olivia, Emily, Isla, Sophie, Ella, Ava, Amelia, Grace, Freya, Charlotte, Jessica, Lucy, Ellie, Sophia, Aria
+Lily, Harper, Mia, Rosie, Millie, Evie, Eilidh, Ruby, Willow, Anna, Maisie, Hannah, Eva, Chloe, Mila, Orla, Isabella, Ivy, Emma
+Georgia, Poppy, Robyn, Daisy, Zara, Gracie, Holly, Skye, Esme, Sofia, Erin, Hallie, Molly, Ayla, Emilia, Layla, Katie, Sienna
+Niamh, Alice, Amber, Bonnie, Maya, Zoe, Ada, Hollie, Bella, Luna, Thea, Rose, Abigail, Summer, Callie, Hope, Lexi, Iona, Elsie
+Leah, Scarlett, Julia, Violet, Myla, Harley, Eve, Imogen, Elizabeth, Cora, Florence, Georgie, Lilly, Matilda, Mirren, Phoebe
+Rowan, Lola, Aurora, Evelyn, Brooke, Clara
+]
+
+both_names = str.[Jack, Oliver, James, Charlie, Harris, Lewis, Leo, Noah, Alfie, Rory, Alexander, Max, Logan, Lucas, Harry, 
+Theo, Thomas, Brodie, Archie, Jacob, Finlay, Finn, Daniel, Joshua, Oscar, Arthur, Hunter, Ethan, Mason, Harrison, Freddie, 
+Ollie, Adam, William, Jaxon, Aaron, Cameron, Liam, George, Jamie, Callum, Matthew, Muhammad, Caleb, Nathan, Tommy, Carter, 
+Blake, Andrew, Luke, Jude, Angus, Riley, Luca, Samuel, Joseph, David, Isaac, Ryan, Hamish, Sonny, Arlo, Arran, Cole, 
+Robert, Blair, Dylan, Louie, Ruaridh, Connor, Benjamin, Kai, Michael, Jackson, Leon, Cooper, Louis, Theodore, 
+Fraser, Owen, Reuben, John, Carson, Innes, Elijah, Murray, Grayson, Aiden, Alex, Cody, Elliot, Ben, Henry,
+Olivia, Emily, Isla, Sophie, Ella, Ava, Amelia, Grace, Freya, Charlotte, Jessica, Lucy, Ellie, Sophia, Aria
+Lily, Harper, Mia, Rosie, Millie, Evie, Eilidh, Ruby, Willow, Anna, Maisie, Hannah, Eva, Chloe, Mila, Orla, Isabella, Ivy, Emma
+Georgia, Poppy, Robyn, Daisy, Zara, Gracie, Holly, Skye, Esme, Sofia, Erin, Hallie, Molly, Ayla, Emilia, Layla, Katie, Sienna
+Niamh, Alice, Amber, Bonnie, Maya, Zoe, Ada, Hollie, Bella, Luna, Thea, Rose, Abigail, Summer, Callie, Hope, Lexi, Iona, Elsie
+Leah, Scarlett, Julia, Violet, Myla, Harley, Eve, Imogen, Elizabeth, Cora, Florence, Georgie, Lilly, Matilda, Mirren, Phoebe
+Rowan, Lola, Aurora, Evelyn, Brooke, Clara
+]
