2ac0755402bdfe6b82daee9da6f8a3577faf75b1
edwardgurney <edwardgurney1979@gmail.com>
2021-08-19 09:22:55 +0000

    Add first two functions, intro and start

--- run.py
+++ run.py
@@ -1,3 +1,31 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import name
+
+"""
+Introduction to the game that seeks user input asking if they want to play
+"""
+def introduction():
+    print("Welcome to the Baby Name Matching Robot. I'm here")
+    print("to help you pick a name for your new baby.\n")
+    print("Would you like to start a matching session?\n")
+    
+    intro_answer = input("Please type 'y' for yes or 'n' for no: ")
+
+    if intro_answer == 'y':
+            start()
+    elif intro_answer == 'n':
+            print("Oh that's too bad... maybe next time.")
+    elif intro_answer != 'y' or 'n':
+            print("That is not a valid input, system will now close...")
+       
+def start():
+    user1 = input("What is the name of user 1?: ")
+    user2 = input("Thanks, can I now have the name of user 2 please?: ")
+
+    print(f"Hello {user1} and {user2}, are you ready to see which")
+    print("baby names you both like?")
+    print(f"{user1}, you will go first, {user2}, you will go second.")
+    print("When the baby names appear, please type 'y' for yes and 'n' fo no\n")
+    print(f"{user1}, are you ready, your names are coming...")
+
+
+introduction()
